d9d37e34eaa532a009860ec606aa17899d7e8add
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-11 11:00:35 +0630

    Feat : Add Product Model

---
+++ PointOfSale.Domain/Models/Product/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Domain.Models.Product
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string ProductCode { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string ProductCategoryCode { get; set; } = null!;
+
+        public bool? DeleteFlag { get; set; }
+
+    }
+}
