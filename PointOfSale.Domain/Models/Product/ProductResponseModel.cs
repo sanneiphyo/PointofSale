3a9ed7301c1ffc163666f2852c6f6e6804610d7e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-12 22:52:31 +0630

    Format Code

--- PointOfSale.Domain/Models/Product/ProductResponseModel.cs
+++ PointOfSale.Domain/Models/Product/ProductResponseModel.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PointOfSale.Domain.Models.Product;
 
-namespace PointOfSale.Domain.Models.Product
+public class ProductResponseModel
 {
-    public class ProductResponseModel
-    {
-        public string ProductCode { get; set; } = null!;
+    public string ProductCode { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        public decimal Price { get; set; }
+    public decimal Price { get; set; }
 
-        public string ProductCategoryCode { get; set; } = null!;
-    }
+    public string ProductCategoryCode { get; set; } = null!;
 }
