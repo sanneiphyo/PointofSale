314ab34e8a3d11de5e86e032ca3f8fedb8c327a1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-12 22:32:04 +0630

    -

--- PointOfSale.DataBase/AppDbContextModels/AppDbContext.cs
+++ PointOfSale.DataBase/AppDbContextModels/AppDbContext.cs
@@ -8,6 +8,8 @@ public partial class AppDbContext : DbContext
     {
     }
 
+    #region DbSet
+
     public virtual DbSet<TblProduct> TblSale { get; set; }
 
     public virtual DbSet<TblProductCategory> TblProductCategories { get; set; }
@@ -15,6 +17,8 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<TblSale> TblSales { get; set; }
 
     public virtual DbSet<TblSaleDetail> TblSaleDetails { get; set; }
+
+    #endregion
 
     //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
