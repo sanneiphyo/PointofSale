12847dc92e4fc020f4e57ea0c02885eb8b483345
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-12 22:26:09 +0630

    Format Code

--- PointofSale/Program.cs
+++ PointofSale/Program.cs
@@ -1,15 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using PointOfSale.DataBase.AppDbContextModels;
 using PointOfSale.Domain.Features.Product;
 using PointOfSale.Domain.Features.Sale;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,7 +21,6 @@ builder.Services.AddScoped<SaleDetailsService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
