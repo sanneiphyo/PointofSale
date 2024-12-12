3c2940da16283a4bc2f03f68e587d20e575ec83e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-12 22:53:06 +0630

    Format Code

--- PointOfSale.Domain/Models/Product/ProductModel.cs
+++ PointOfSale.Domain/Models/Product/ProductModel.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PointOfSale.Domain.Models.Product;
 
-namespace PointOfSale.Domain.Models.Product
+public class ProductModel
 {
-    public class ProductModel
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string ProductCode { get; set; } = null!;
+    public string ProductCode { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        public decimal Price { get; set; }
+    public decimal Price { get; set; }
 
-        public string ProductCategoryCode { get; set; } = null!;
+    public string ProductCategoryCode { get; set; } = null!;
 
-        public bool? DeleteFlag { get; set; }
+    public bool? DeleteFlag { get; set; }
 
-    }
 }
