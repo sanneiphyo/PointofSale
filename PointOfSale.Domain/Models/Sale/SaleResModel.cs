09fd9ae74016b7095221f0d43ee262a89be90a58
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:09:18 +0630

    -

--- PointOfSale.Domain/Models/Sale/SaleResModel.cs
+++ PointOfSale.Domain/Models/Sale/SaleResModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PointOfSale.DataBase.AppDbContextModels;
-
-namespace PointOfSale.Domain.Models.Sale
+﻿namespace PointOfSale.Domain.Models.Sale
 {
    public class ResultSaleModel
     {
