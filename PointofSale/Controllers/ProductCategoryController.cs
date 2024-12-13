e43f762ca79c8c1e9d9dd217990c161682756531
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:25:22 +0630

    Format Code

--- PointofSale/Controllers/ProductCategoryController.cs
+++ PointofSale/Controllers/ProductCategoryController.cs
@@ -1,65 +1,64 @@
-﻿namespace PointofSale.RestApi.Controllers
+﻿namespace PointofSale.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductCategoryController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductCategoryController : ControllerBase
+
+    public readonly ProductCategoryService _service;
+
+    public ProductCategoryController(ProductCategoryService service)
     {
-
-        public readonly ProductCategoryService _service;
-
-        public ProductCategoryController(ProductCategoryService service)
-        {
-            _service = service;
-        }
-        
-
-        [HttpPost("Create")]
-        public async Task<IActionResult> CreateProductCategoryAsync([FromBody] ProductCategoryReqModel reqModel)
-        {
-            try
-            {
-                var result = await _service.CreateProductCategoryAsync(reqModel);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-
-                return StatusCode(500, new { error = ex.Message });
-            }
-        }
-
-  
-
-        [HttpGet("Get")]
-        public async Task<IActionResult> GetProductCategoryAsync()
-        {
-            try
-            {
-                var lst = await _service.GetProductCategoryAsync();
-                return Ok(lst);
-            }
-            catch (Exception ex)
-            {
-
-                return StatusCode(500, new { error = ex.Message });
-            }
-        }
-
-
-        [HttpPut("{productCategoryCode}")]
-        public async Task<IActionResult> UpdateProductCategoryAsync(string productCategoryCode, ProductCategoryResModel resModel)
-        {
-            try
-            {
-                var item = await _service.UpdateProductCategoryAsync(productCategoryCode, resModel);
-                return Ok(item);
-            }
-            catch (Exception ex)
-            {
-
-                return StatusCode(500, new { error = ex.Message });
-            }
-        }
-
+        _service = service;
     }
+    
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> CreateProductCategoryAsync([FromBody] ProductCategoryReqModel reqModel)
+    {
+        try
+        {
+            var result = await _service.CreateProductCategoryAsync(reqModel);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(500, new { error = ex.Message });
+        }
+    }
+
+
+
+    [HttpGet("Get")]
+    public async Task<IActionResult> GetProductCategoryAsync()
+    {
+        try
+        {
+            var lst = await _service.GetProductCategoryAsync();
+            return Ok(lst);
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(500, new { error = ex.Message });
+        }
+    }
+
+
+    [HttpPut("{productCategoryCode}")]
+    public async Task<IActionResult> UpdateProductCategoryAsync(string productCategoryCode, ProductCategoryResModel resModel)
+    {
+        try
+        {
+            var item = await _service.UpdateProductCategoryAsync(productCategoryCode, resModel);
+            return Ok(item);
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(500, new { error = ex.Message });
+        }
+    }
+
 }
