3ae037be6c99f149203e6989b9b35f0ec774e22f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:03:02 +0630

    Feat : Add CreateProductCategoryAsync in Controller

---
+++ PointofSale/Controllers/ProductCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using PointOfSale.Domain.Features.ProductCategory;
+using PointOfSale.Domain.Models.ProductCategory;
+
+namespace PointofSale.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+
+        public readonly ProductCategoryService _service;
+
+        public ProductCategoryController(ProductCategoryService service)
+        {
+            _service = service;
+        }
+        
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateProductCategoryAsync([FromBody] ProductCategoryReqModel reqModel)
+        {
+            try
+            {
+                var result = await _service.CreateProductCategoryAsync(reqModel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+  
+
+
+    }
+}
