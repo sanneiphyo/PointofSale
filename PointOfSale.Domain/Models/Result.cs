f3eae0def3309873de8581eb29a33cc3429ca95e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:06:46 +0630

    -

--- PointOfSale.Domain/Models/Result.cs
+++ PointOfSale.Domain/Models/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PointOfSale.Domain.Models
+﻿namespace PointOfSale.Domain.Models
 {
     public class Result<T>
     {
