b304010b86f22ff89f2be2c31f90aa4158436e52
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:06:58 +0630

    Format Code

--- PointOfSale.Domain/Models/Result.cs
+++ PointOfSale.Domain/Models/Result.cs
@@ -1,66 +1,65 @@
-﻿namespace PointOfSale.Domain.Models
+﻿namespace PointOfSale.Domain.Models;
+
+public class Result<T>
 {
-    public class Result<T>
+    public bool IsSuccess { get; set; }
+    public bool IsError { get { return !IsSuccess; } }
+    public bool IsValidationError { get { return Type == EnumRespType.ValidationError; } }
+    public bool IsSystemError { get { return Type == EnumRespType.SystemError; } }
+
+    private EnumRespType Type { get; set; }
+    public T Data { get; set; }
+    public string Message { get; set; }
+
+    public static Result<T> Success(T data, string message = "Success")
     {
-        public bool IsSuccess { get; set; }
-        public bool IsError { get { return !IsSuccess; } }
-        public bool IsValidationError { get { return Type == EnumRespType.ValidationError; } }
-        public bool IsSystemError { get { return Type == EnumRespType.SystemError; } }
-
-        private EnumRespType Type { get; set; }
-        public T Data { get; set; }
-        public string Message { get; set; }
-
-        public static Result<T> Success(T data, string message = "Success")
+        return new Result<T>()
         {
-            return new Result<T>()
-            {
-                IsSuccess = true,
-                Type = EnumRespType.Success,
-                Data = data,
-                Message = message
-            };
-        }
-
-        public static Result<T> DeleteSuccess(string message = "Deleting Successful.")
-        {
-            return new Result<T>()
-            {
-                IsSuccess = true,
-                Type = EnumRespType.Success,
-                Message = message
-            };
-        }
-
-        public static Result<T> ValidationError(string message, T? data = default)
-        {
-            return new Result<T>()
-            {
-                IsSuccess = true,
-                Type = EnumRespType.ValidationError,
-                Data = data,
-                Message = message
-            };
-        }
-
-        public static Result<T> SystemError(string message, T? data = default)
-        {
-            return new Result<T>()
-            {
-                IsSuccess = true,
-                Type = EnumRespType.SystemError,
-                Data = data,
-                Message = message
-            };
-        }
-
-        public enum EnumRespType
-        {
-            None,
-            Success,
-            ValidationError,
-            SystemError
-        }
-
+            IsSuccess = true,
+            Type = EnumRespType.Success,
+            Data = data,
+            Message = message
+        };
     }
+
+    public static Result<T> DeleteSuccess(string message = "Deleting Successful.")
+    {
+        return new Result<T>()
+        {
+            IsSuccess = true,
+            Type = EnumRespType.Success,
+            Message = message
+        };
+    }
+
+    public static Result<T> ValidationError(string message, T? data = default)
+    {
+        return new Result<T>()
+        {
+            IsSuccess = true,
+            Type = EnumRespType.ValidationError,
+            Data = data,
+            Message = message
+        };
+    }
+
+    public static Result<T> SystemError(string message, T? data = default)
+    {
+        return new Result<T>()
+        {
+            IsSuccess = true,
+            Type = EnumRespType.SystemError,
+            Data = data,
+            Message = message
+        };
+    }
+
+    public enum EnumRespType
+    {
+        None,
+        Success,
+        ValidationError,
+        SystemError
+    }
+
 }
