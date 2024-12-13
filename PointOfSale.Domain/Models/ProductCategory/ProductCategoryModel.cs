339f2df49dd014351e257cdef2f86e1180466223
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:12:47 +0630

    Format Code

--- PointOfSale.Domain/Models/ProductCategory/ProductCategoryModel.cs
+++ PointOfSale.Domain/Models/ProductCategory/ProductCategoryModel.cs
@@ -1,14 +1,16 @@
-﻿namespace PointOfSale.Domain.Models.ProductCategory
+﻿namespace PointOfSale.Domain.Models.ProductCategory;
+
+#region ProductCategoryModel
+
+public class ProductCategoryModel
 {
-    public class ProductCategoryModel
-    {
-        public int ProductCategoryId { get; set; }
+    public int ProductCategoryId { get; set; }
 
-        public string ProductCategoryCode { get; set; } = null!;
+    public string ProductCategoryCode { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        public bool? DeleteFlag { get; set; }
-
-    }
+    public bool? DeleteFlag { get; set; }
 }
+
+#endregion
