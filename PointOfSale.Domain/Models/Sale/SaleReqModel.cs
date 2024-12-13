7956afce50e1beed90b0b471c2298c9aad8f1eae
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:11:09 +0630

    -

--- PointOfSale.Domain/Models/Sale/SaleReqModel.cs
+++ PointOfSale.Domain/Models/Sale/SaleReqModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace PointOfSale.Domain.Models.Sale;
+
+#region SaleRequestModel
+
 public class SaleRequestModel
 {
     public TblSale? Sale { get; set; }
     public TblSaleDetail? SaleDetail { get; set; }
 }
+
+#endregion
