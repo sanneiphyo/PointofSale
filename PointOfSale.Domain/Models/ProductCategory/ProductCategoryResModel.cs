1ba8ff55a8e03fb356381c16a654e259e153abd8
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:01:50 +0630

    Feat : Update ProductCategoryResModel

--- PointOfSale.Domain/Models/ProductCategory/ProductCategoryResModel.cs
+++ PointOfSale.Domain/Models/ProductCategory/ProductCategoryResModel.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PointOfSale.DataBase.AppDbContextModels;
-
-namespace PointOfSale.Domain.Models.ProductCategory
+﻿namespace PointOfSale.Domain.Models.ProductCategory
 {
-    public class ResultProductCategoryResponseModel
+    public class ProductCategoryResModel
     {
-        public TblProductCategory? ProductCategory { get; set; }
+        public string Name { get; set; } = null!;
     }
 }
 
