3e18dcf79ce75dfd2a4feeac12fac5749bb83631
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:09:31 +0630

    Format Code

--- PointOfSale.Domain/Models/Sale/SaleResModel.cs
+++ PointOfSale.Domain/Models/Sale/SaleResModel.cs
@@ -1,16 +1,14 @@
-﻿namespace PointOfSale.Domain.Models.Sale
+﻿namespace PointOfSale.Domain.Models.Sale;
+
+public class ResultSaleModel
 {
-   public class ResultSaleModel
-    {
-       public TblSale? Sale{ get; set; }
-        
+   public TblSale? Sale{ get; set; }
+    
 
-    }
+}
 
-    public class ResultSaleDetailModel
-    {
-        public TblSaleDetail? Detail { get; set; }
-
-    }
+public class ResultSaleDetailModel
+{
+    public TblSaleDetail? Detail { get; set; }
 
 }
