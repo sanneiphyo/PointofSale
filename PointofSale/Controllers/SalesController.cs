e6f8e6ce2ee1c55668db603c642a3840acdfaffd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:23:20 +0630

    Format Code

--- PointofSale/Controllers/SalesController.cs
+++ PointofSale/Controllers/SalesController.cs
@@ -1,83 +1,82 @@
-﻿namespace PointofSale.RestApi.Controllers
+﻿namespace PointofSale.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SalesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SalesController : ControllerBase
+    private readonly SaleService _saleService;
+    private readonly SaleDetailsService _saleDetailsService;
+
+    public SalesController(SaleService saleService, SaleDetailsService saleDetailsService)
     {
-        private readonly SaleService _saleService;
-        private readonly SaleDetailsService _saleDetailsService;
+        _saleService = saleService;
+        _saleDetailsService = saleDetailsService;
+    }
 
-        public SalesController(SaleService saleService, SaleDetailsService saleDetailsService)
+    [HttpGet("get-sale-by-voucher")]
+    public async Task<IActionResult> GetSale(string voucherNo)
+    {
+        try
         {
-            _saleService = saleService;
-            _saleDetailsService = saleDetailsService;
+            var lst = await _saleService.GetSaleAsync(voucherNo);
+            var saleDetailResult = await _saleDetailsService.GetSaleDetailAsync(voucherNo);
+
+            return Ok(new
+            {
+                SaleDetail = lst,
+                SaleDetailResult = saleDetailResult
+            });
         }
-
-        [HttpGet("get-sale-by-voucher")]
-        public async Task<IActionResult> GetSale(string voucherNo)
+        catch (Exception ex)
         {
-            try
-            {
-                var lst = await _saleService.GetSaleAsync(voucherNo);
-                var saleDetailResult = await _saleDetailsService.GetSaleDetailAsync(voucherNo);
 
-                return Ok(new
-                {
-                    SaleDetail = lst,
-                    SaleDetailResult = saleDetailResult
-                });
-            }
-            catch (Exception ex)
-            {
-
-                return StatusCode(500, new { error = ex.Message });
-            }
+            return StatusCode(500, new { error = ex.Message });
         }
+    }
 
-        //[HttpPost("sale")]
-        //public async Task<IActionResult> CreateSale(SaleRequestModel reqModel)
-        //{
-        //    try
-        //    {
-        //        TblSale sale = reqModel.Sale!;
-        //        TblSaleDetail saleDetail = reqModel.SaleDetail!;
+    //[HttpPost("sale")]
+    //public async Task<IActionResult> CreateSale(SaleRequestModel reqModel)
+    //{
+    //    try
+    //    {
+    //        TblSale sale = reqModel.Sale!;
+    //        TblSaleDetail saleDetail = reqModel.SaleDetail!;
 
-        //        var item = await _saleService.CreateSaleAsync(sale);
-        //        var result = await _saleDetailsService.CreateSaleDetailAsync(saleDetail);
+    //        var item = await _saleService.CreateSaleAsync(sale);
+    //        var result = await _saleDetailsService.CreateSaleDetailAsync(saleDetail);
 
-        //        return Ok(result);
-        //    }
-        //    catch (Exception ex)
-        //    {
+    //        return Ok(result);
+    //    }
+    //    catch (Exception ex)
+    //    {
 
-        //        return StatusCode(500, new { error = ex.Message });
-        //    }
-        //}
+    //        return StatusCode(500, new { error = ex.Message });
+    //    }
+    //}
 
-        [HttpPost("sale")]
-        public async Task<IActionResult> CreateSale(SaleRequestModel reqModel)
+    [HttpPost("sale")]
+    public async Task<IActionResult> CreateSale(SaleRequestModel reqModel)
+    {
+        try
         {
-            try
-            {
-            
-                TblSale sale = reqModel.Sale!;
-                TblSaleDetail saleDetail = reqModel.SaleDetail!;
+        
+            TblSale sale = reqModel.Sale!;
+            TblSaleDetail saleDetail = reqModel.SaleDetail!;
 
-              
-                var saleResult = await _saleService.CreateSaleAsync(sale);
-              
-                var saleDetailResult = await _saleDetailsService.CreateSaleDetailAsync(saleDetail);
-             
-                return Ok(new
-                {
-                    Sale = saleResult.Data,
-                    SaleDetail = saleDetailResult.Data
-                });
-            }
-            catch (Exception ex)
+          
+            var saleResult = await _saleService.CreateSaleAsync(sale);
+          
+            var saleDetailResult = await _saleDetailsService.CreateSaleDetailAsync(saleDetail);
+         
+            return Ok(new
             {
-                return StatusCode(500, new { error = ex.Message });
-            }
+                Sale = saleResult.Data,
+                SaleDetail = saleDetailResult.Data
+            });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = ex.Message });
         }
     }
 }
