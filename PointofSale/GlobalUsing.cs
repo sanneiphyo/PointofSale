6a7ee75f10c12402916967116d8cab68057a0acc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 02:22:36 +0630

    Feat : Add Global Using

--- PointofSale/GlobalUsing.cs
+++ PointofSale/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using PointOfSale.Domain.Models.Product;
 global using Microsoft.EntityFrameworkCore;
 global using PointOfSale.DataBase.AppDbContextModels;
 global using PointOfSale.Domain.Features.Sale;
+global using PointOfSale.Domain.Features.ProductCategory;
